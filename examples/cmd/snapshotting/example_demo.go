d57cfa7228be752965725c2acad10a4357bb2550
Tamal Saha <tamal@appscode.com>
2022-10-31 11:03:52 -0400

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- examples/cmd/snapshotting/example_demo.go
+++ examples/cmd/snapshotting/example_demo.go
@@ -286,14 +286,14 @@ func createSnapshotSSH(ctx context.Context, instanceID int, socketPath, memPath,
 				// ds=nocloud-net;s=http://169.254.169.254/latest/
 				// network-config=__NETWORK_CONFIG__",
 
-				ds := fmt.Sprintf("nocloud-net;s=http://%s/latest/", MMDS_IP)
-				kernelArgs["ds"] = &ds
+				//ds := fmt.Sprintf("nocloud-net;s=http://%s/latest/", MMDS_IP)
+				//kernelArgs["ds"] = &ds
 
-				netcfg, err := BuildNetCfg(eth0Mac, eth1Mac, ip0, ip1)
-				if err != nil {
-					return err
-				}
-				kernelArgs["network-config"] = &netcfg
+				//netcfg, err := BuildNetCfg(eth0Mac, eth1Mac, ip0, ip1)
+				//if err != nil {
+				//	return err
+				//}
+				//kernelArgs["network-config"] = &netcfg
 
 				ipBootParam := func(conf *sdk.IPConfiguration) string {
 					// the vmconf package already has a function for doing this, just re-use it
@@ -312,7 +312,7 @@ func createSnapshotSSH(ctx context.Context, instanceID int, socketPath, memPath,
 				// The Ethernet device eth0 will be automatically configured using BOOTP.
 				ipBootParam2 = strings.ReplaceAll(ipBootParam2, ":off:", ":bootp:")
 				oneliners.FILE("IP:", ipBootParam2)
-				// kernelArgs["ip"] = &ipBootParam2
+				kernelArgs["ip"] = &ipBootParam2
 
 				m.Cfg.KernelArgs = `console=ttyS0 noapic reboot=k panic=1 pci=off nomodules rw ` + kernelArgs.String()
 
